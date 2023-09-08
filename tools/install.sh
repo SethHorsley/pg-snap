7c9df7105205f87d957d030b990395a6a6dfe14a
Seth Horsley <isethi@me.com>
2023-09-08 04:29:55 -0400

    add sudo message to install script

--- tools/install.sh
+++ tools/install.sh
@@ -6,4 +6,8 @@ mkdir -p /usr/local/bin
 
 # copy pg_snap to /usr/local/bin
 curl -s $GITHUB_URL -o /usr/local/bin/pg_snap
+
+# make pg_snap executable
+echo "We require sudo to make pg_snap executable"
+echo "Check for yourself before running it: https://github.com/iseth/pg-snap/blob/master/tools/install.sh#L13"
 sudo chmod +x /usr/local/bin/pg_snap
