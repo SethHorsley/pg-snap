61e29e024e70ceb27fe4a1117f8129d658b0b9ee
Seth Horsley <isethi@me.com>
2023-09-18 09:17:36 -0400

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -8,9 +8,9 @@ GITHUB_URL="https://raw.githubusercontent.com/iseth/pg-snap/master/pg_snap"
 mkdir -p /usr/local/bin
 
 # copy pg_snap to /usr/local/bin
-curl -s $GITHUB_URL -o /usr/local/bin/pg_snap
+sudo curl -s $GITHUB_URL -o /usr/local/bin/pg_snap
 
 # make pg_snap executable
 echo "We require sudo to make pg_snap executable"
 echo "Check for yourself before running it: https://github.com/iseth/pg-snap/blob/master/tools/install.sh#L4"
-chmod +x /usr/local/bin/pg_snap
+sudo chmod +x /usr/local/bin/pg_snap
